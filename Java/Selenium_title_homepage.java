475c1ce9bafc4cb6342c2052f9010028847103c4
Politehamster <42449275+Politehamster@users.noreply.github.com>
2020-07-31 17:26:22 +0600

    Commit != Push
    Yeah.

--- Java/Selenium_title_homepage.java
+++ Java/Selenium_title_homepage.java
@@ -8,7 +8,7 @@ public class PG1 {
     public static void main(String[] args) {
     	//System.setProperty("webdriver.gecko.driver","C:\\geckodriver.exe");
 	//WebDriver driver = new FirefoxDriver();
-	System.setProperty("webdriver.chrome.driver","H:\\chromedriver.exe");
+	System.setProperty("webdriver.chrome.driver", ".\\Driver\\chromedriver.exe");
 	WebDriver driver = new ChromeDriver();
     	
         String baseUrl = "http://demo.guru99.com/test/newtours/";
